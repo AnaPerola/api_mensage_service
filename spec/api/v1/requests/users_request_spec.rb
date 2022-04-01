79c285702c2f88417b6b5d17af6213092a2b4b92
Ana Perola <anaperolinha@hotmail.com>
2022-04-01 11:59:31 -0300

    Created test requests user and fix test home

---
+++ spec/api/v1/requests/users_request_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Visitor accesses Users' do
+  it 'accesses /GET /api/v1/users' do
+    get '/api/v1/users'
+    expect(response.status).to eq(200)
+    # expect(response.body).to eq('{user}')
+  end
+
+  it 'accesses /POST /api/v1/users' do
+    post '/api/v1/users', params: {
+      user: { name: 'ana', email: 'ana@example.com' }
+    }
+    expect(response.status).to eq(201)
+    json = JSON.parse(response.body).deep_symbolize_keys
+    expect(json[:name]).to eq('ana')
+    expect(json[:email]).to eq('ana@example.com')
+  end
+
+  it 'accesses /DELETE /api/v1/users/{id}' do
+    user = User.create(name: 'ana', email: 'ana@example.com')
+    delete "/api/v1/users/#{user.id}"
+    expect(response.status).to eq(200)
+  end
+
+  it 'accesses /PUT /api/v1/users/{id}'
+end
--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -2,11 +2,11 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Visitor accesses home', type: :request do
+RSpec.describe 'Visitor accesses home' do
   describe 'GET /show' do
     it 'returns http success' do
       get '/'
-      expect(response.body).to eq('{"rails_version":"7.0.2.3","ruby_version":"3.0.3","rails_environment":"test","developer":"Ana Pérola"} ')
+      expect(response.body).to eq('{"rails_version":"7.0.2.3","ruby_version":"3.0.3","rails_environment":"test","developer":"Ana Pérola"}')
       expect(response.status).to eq(200)
     end
   end
