6e814ea42fd1ee46e7ca79f0251c869f17daad15
Ana Perola <anaperolinha@hotmail.com>
2022-04-04 13:20:27 -0300

    Create route to action cable in development

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   root to: 'home#show'
+
+  mount ActionCable.server => "/cable"
+  
   namespace :api, default: { format: :json } do
     namespace :v1 do
       resources :users 
