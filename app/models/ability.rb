8b3dfea68a74b1fa32237ee742c041f6c19345c7
Ana Perola <anaperolinha@hotmail.com>
2022-04-04 13:15:49 -0300

    Add config to authenticate

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,12 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-      user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can [:read, :destroy], :all
-      else
-        can :read, :all
-      end
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can %i[read destroy], :all
+    else
+      can :read, :all
+    end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
